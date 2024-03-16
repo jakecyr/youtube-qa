65d9d5bfff6805c26851f37ed4d0ff5b89a32364
Jake Cyr <cyrjake@gmail.com>
2024-03-15 23:44:20 -0400

    Split up files

--- examples/workout.py
+++ examples/workout.py
@@ -1,4 +1,7 @@
 from youtube_qa.answer_question import answer_question_using_youtube
+from dotenv import load_dotenv
+
+load_dotenv()
 
 response, sources = answer_question_using_youtube(
     search_term="peter attia running endurance",
